f67cb7660015fe80e0a6871ca027dc6c3a73f69f
ichea88@gmail.com <ichea88@gmail.com@c4a33fc5-b5dd-497b-6068-1783f4a6d9b2>
2012-01-17 10:50:24 +0000

    From Float to BigDecimal

--- iaproject-2dbpp/src/core/taboo/TabooList.java
+++ iaproject-2dbpp/src/core/taboo/TabooList.java
@@ -1,11 +1,14 @@
 package core.taboo;
 
+import java.math.BigDecimal;
 import java.util.LinkedList;
 
 public class TabooList {
 	
 	private final int tenure;
-	private final LinkedList<Float> moves = new LinkedList<Float>();
+	//private final LinkedList<Float> moves = new LinkedList<Float>();
+	private final LinkedList<BigDecimal> moves = new LinkedList<BigDecimal>();
+	private final int precision = 4;
 	
 	public TabooList(int tenure) {
 		if (tenure <= 0) {
@@ -16,7 +19,8 @@ public class TabooList {
 	}
 	
 	public boolean isTabu(float move) {
-		return moves.contains(Float.valueOf(move));
+		//return moves.contains(Float.valueOf(move));
+		return moves.contains(new BigDecimal(move).setScale(precision, BigDecimal.ROUND_HALF_UP));
 	}
 	
 	public void addMove(float move) {
@@ -26,7 +30,8 @@ public class TabooList {
 			moves.removeLast();
 		}
 		
-		moves.addFirst(Float.valueOf(move));
+		//moves.addFirst(Float.valueOf(move));
+		moves.addFirst(new BigDecimal(move).setScale(precision, BigDecimal.ROUND_HALF_UP));
 	}
 	
 	public void clear() {
